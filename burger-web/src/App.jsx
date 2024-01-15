1bce7501ebb5cd04d4309cae7d14eab2753bca65
supunbb <125901930+supunbb@users.noreply.github.com>
2024-01-15 14:32:47 +0530

    initial commit

---
+++ burger-web/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <div className=' container bg-primary text-white'><Navbar/></div>
+  )
+}
+
+export default App
